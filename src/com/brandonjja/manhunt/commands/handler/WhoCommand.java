058549dd7e571de44a485533ab2bbf1bf8551dc5
Brandon <brandonjja@hotmail.com>
2020-09-24 00:24:47 -0300

    Updated string formatting to make it more readable

--- src/com/brandonjja/manhunt/commands/handler/WhoCommand.java
+++ src/com/brandonjja/manhunt/commands/handler/WhoCommand.java
@@ -1,6 +1,7 @@
 package com.brandonjja.manhunt.commands.handler;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import com.brandonjja.manhunt.ManHunt;
@@ -12,15 +13,15 @@ public class WhoCommand extends ManHuntCommand {
 	@Override
 	public boolean execute(Player player, String[] args) {
 		PlayerMH mhPlayer;
-		String list = "";
+		String list = ChatColor.GOLD + "------ Players: ------\n";
 		for (Player pl : Bukkit.getOnlinePlayers()) {
 			mhPlayer = ManHunt.getPlayer(pl);
-			list += pl.getName() + " - " + mhPlayer.getRole() + "\n";
+			list += ChatColor.GREEN + pl.getName() + ChatColor.WHITE + " - " + ChatColor.AQUA + mhPlayer.getRole() + "\n";
 		}
+		list += ChatColor.GOLD + "--------------------";
 		
 		player.sendMessage(list);
 		
 		return true;
 	}
-	
 }
